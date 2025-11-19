63afb2a86785a9a1e417b13204083cf8d9198409
Your Name <you@example.com>
2025-11-20 07:18:47 +0800

    Fix fan draw sensor origin

--- capture_d435_pcd_safe_roi.py
+++ capture_d435_pcd_safe_roi.py
@@ -182,7 +182,8 @@ def main():
     occ = _apply_morphology(occ)
 
     occ_rgb = cv2.cvtColor(occ, cv2.COLOR_GRAY2BGR)
-    draw_backward_fan_reference(occ_rgb)
+    sensor_rc = (robot_row, robot_col)
+    draw_backward_fan_reference(occ_rgb, sensor_rc)
     _draw_grid(occ_rgb)
     robot_col = int(round((0.0 - WORLD_X[0]) / CELL_M))
     robot_row = int(round((WORLD_Y[1] - 0.0) / CELL_M))
